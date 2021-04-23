485a30b93c1c33ce231a56f7b9e5a5c72b94b59f
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-04-24 04:12:03 +0800

    Initial Commit

---
+++ shopp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopp
+//
+//  Created by 簡吟真 on 2021/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
